9a251a53762b50e07bf2f7f597d0d4cf830916ad
Paul Miller <pmiller@microsoft.com>
2018-06-21 13:41:03 -0700

    try and instrument

--- Program.cs
+++ Program.cs
@@ -2,8 +2,11 @@
 using System.IO;
 using System.Linq;
 using System.Diagnostics;
+using System.Collections.Generic;
 using Newtonsoft.Json;
 using CommandLine;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility;
 
 namespace DropDownloadCore
 {
@@ -17,21 +20,39 @@ namespace DropDownloadCore
 
         static void Run(Args a)
         {
-            a.ValidatePat();
-            var url = a.DropUrl ?? ExtractDropUrl(a.DropDestination);
-
-            // sample URL:
-            // https://msasg.artifacts.visualstudio.com/DefaultCollection/_apis/drop/drops/Aether_master/7dd31c59986465bfa9af3bd883cb35ce132979a2/e90d7f94-265a-86c7-5958-66983fdcaa06
-            Console.WriteLine($"url: {url}");
-            // /Release/Amd64/app/aether/AetherBackend
-            Console.WriteLine($"relative path: {a.RelativePath}");
-            Console.WriteLine($"destination: {a.DropDestination}");
-            var proxy = new VSTSDropProxy(url, a.RelativePath, a.VstsPat);
+            var telemetry = new TelemetryClient(new TelemetryConfiguration(a.InstrumentationKey));
+            //https://docs.microsoft.com/en-us/dotnet/api/microsoft.applicationinsights.telemetryclient.trackevent?view=azure-dotnet
+            var props  = new Dictionary<string,string>();
+            var metrics = new Dictionary<string,double>();
             var sw = Stopwatch.StartNew();
-            proxy.Materialize(a.DropDestination).Wait();
-            sw.Stop();
-
-            Console.WriteLine($"Finished in {sw.Elapsed}");
+            try
+            {
+                a.ValidatePat();
+                var url = a.DropUrl ?? ExtractDropUrl(a.DropDestination);
+                props["url"] = url;
+                // sample URL:
+                // https://msasg.artifacts.visualstudio.com/DefaultCollection/_apis/drop/drops/Aether_master/7dd31c59986465bfa9af3bd883cb35ce132979a2/e90d7f94-265a-86c7-5958-66983fdcaa06
+                Console.WriteLine($"url: {url}");
+                // /Release/Amd64/app/aether/AetherBackend
+                Console.WriteLine($"relative path: {a.RelativePath}");
+                Console.WriteLine($"destination: {a.DropDestination}");
+                var proxy = new VSTSDropProxy(url, a.RelativePath, a.VstsPat);
+                metrics = proxy.Materialize(a.DropDestination).Result;
+                Console.WriteLine($"Finished in {sw.Elapsed}");
+                props["success"] = "True";
+                
+            } 
+            catch (Exception e)
+            {
+                props["success"]  = "False";
+                props["exception"] = e.ToString();
+                throw;
+            }
+            finally
+            {
+                 metrics["Elapsed"] = sw.Elapsed.TotalSeconds;
+                 telemetry.TrackEvent("dropdownloader", props, metrics);
+            }
         }
 
         // agent based tasks automatically download artifacts from the build. 
